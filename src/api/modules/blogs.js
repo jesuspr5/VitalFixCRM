578fd0a5e9412b4391ad570cc6af4991d304ae48
jesuspr5 <pereirajesus35@gmail.com>
2023-01-05 10:18:15 -0400

    carga fotos a azure

--- src/api/modules/blogs.js
+++ src/api/modules/blogs.js
@@ -1,55 +1,84 @@
 import axios from "axios";
 
-
-async function blogsGetList(Page,Rows){
+async function blogsGetList(Page, Rows) {
   let result;
-  result = await axios.get("https://as-humedal-api.azurewebsites.net/Blogs/GetList?Page="+Page+"&Rows="+Rows)
-  return result.data.data.items
+  result = await axios.get(
+    "https://as-humedal-api.azurewebsites.net/Blogs/GetList?Page=" +
+      Page +
+      "&Rows=" +
+      Rows
+  );
+  return result.data.data.items;
 }
 
-async function blogsGetListCatType(Page,Rows,IdCatType){
-    let result;
-    result = await axios.get("https://as-humedal-api.azurewebsites.net/Blogs/GetListCat?Page="+Page+"&Rows="+Rows+"&IdCatTyp="+IdCatType)
-    return result.data.data.items
-  }
-  async function blogsGetListText(Page,Rows,Text){
-    let result;
-    result = await axios.get("https://as-humedal-api.azurewebsites.net/Blogs/GetListText?Page="+Page+"&Rows="+Rows+"&IdCatTyp="+Text)
-    return result.data.data.items
-  }
-
-async function blogsGet(Idblog){
+async function blogsGetListCatType(Page, Rows, IdCatType) {
   let result;
-  result = await axios.get("https://as-humedal-api.azurewebsites.net/Blogs/Get?idBLogLanding="+blog)
-  console.log('GETRESULT: ', result.data.data)
-  return result.data.data
+  result = await axios.get(
+    "https://as-humedal-api.azurewebsites.net/Blogs/GetListCat?Page=" +
+      Page +
+      "&Rows=" +
+      Rows +
+      "&IdCatTyp=" +
+      IdCatType
+  );
+  return result.data.data.items;
 }
-async function createblog( blogToCreate ){
+async function blogsGetListText(Page, Rows, Text) {
   let result;
-    result = await axios.post("https://as-humedal-api.azurewebsites.net/Blogs/Create",blogToCreate)
-    console.log('Usuario creado: ', result)
-    return result
-
-}
-
-async function deleteblog(Idblog){
-  let result;
-    result = await axios.delete("https://as-humedal-api.azurewebsites.net/Blogs/Delete?IdBlogLanding="+ IdblogLanding)
-    console.log('Usuario Eliminado: ', result)
-    return result
-
+  result = await axios.get(
+    "https://as-humedal-api.azurewebsites.net/Blogs/GetListText?Page=" +
+      Page +
+      "&Rows=" +
+      Rows +
+      "&IdCatTyp=" +
+      Text
+  );
+  return result.data.data.items;
 }
 
-async function updateblog( blogToUpdate ){
+async function blogsGet(Idblog) {
   let result;
-    result = await axios.post("https://as-humedal-api.azurewebsites.net/Blogs/Update", blogToUpdate)
-    console.log('Usuario Actualizado: ', result)
-    return result
-
+  result = await axios.get(
+    "https://as-humedal-api.azurewebsites.net/Blogs/Get?idBLogLanding=" + blog
+  );
+  console.log("GETRESULT: ", result.data.data);
+  return result.data.data;
 }
-async function uploadimg(img){
-  let result ;
- result=await axios.post("https://as-humedal-api.azurewebsites.net/Blogs/Upload",img) 
+async function createblog(blogToCreate) {
+  let result;
+  result = await axios.post(
+    "https://as-humedal-api.azurewebsites.net/Blogs/Create",
+    blogToCreate
+  );
+  console.log("Usuario creado: ", result);
+  return result;
+}
+
+async function deleteblog(Idblog) {
+  let result;
+  result = await axios.delete(
+    "https://as-humedal-api.azurewebsites.net/Blogs/Delete?IdBlogLanding=" +
+      IdblogLanding
+  );
+  console.log("Usuario Eliminado: ", result);
+  return result;
+}
+
+async function updateblog(blogToUpdate) {
+  let result;
+  result = await axios.post(
+    "https://as-humedal-api.azurewebsites.net/Blogs/Update",
+    blogToUpdate
+  );
+  console.log("Usuario Actualizado: ", result);
+  return result;
+}
+// async function uploadimg(img){
+//   let result ;
+//  result=await axios.post("https://as-humedal-api.azurewebsites.net/Blogs/Upload",img)
+// }
+ function uploadimg() {
+  return "https://as-humedal-api.azurewebsites.net/Blogs/Upload";
 }
 
 // export const createCharityOrg = (body) =>
@@ -58,8 +87,7 @@ async function uploadimg(img){
 //     Authorization: 'Bearer ' + `${token}`,
 // })
 
-
-export { 
+export {
   blogsGetList,
   blogsGetListCatType,
   blogsGetListText,
@@ -68,4 +96,4 @@ export {
   createblog,
   blogsGet,
   uploadimg
-}
+};
